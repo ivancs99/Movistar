235b9a3b6ad02794945311198938df004ceb915f
nanintaker <59866155+nanintaker@users.noreply.github.com>
2020-01-17 12:21:15 +0100

    AYUDAMEEE AYUDAMEEEE JODER TIOOOO

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
         System.out.println("Hola buenas tardes");
         System.out.println("Soy Juan");
         System.out.println("Ivan es gei");
+        System.out.println("Asies");
     }
     
 }
