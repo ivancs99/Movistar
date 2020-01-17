415f21a4fee4968e04527612ad90c665dd521333
ivancs99 <ivancs99@github.com>
2020-01-17 12:18:06 +0100

    Actualisasio
    Hey

--- ProyectoMovistarIvan/src/proyectomovistarivan/ProyectoMovistarIvan.java
+++ ProyectoMovistarIvan/src/proyectomovistarivan/ProyectoMovistarIvan.java
@@ -6,6 +6,7 @@ public class ProyectoMovistarIvan {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Buenas");
+        System.out.println("Vamos a hacer un chat a base de souts");
     }
     
 }
