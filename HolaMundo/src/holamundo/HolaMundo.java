8193385990ab744eebee1d77dc0c1d5544a32f13
nanintaker <59866155+nanintaker@users.noreply.github.com>
2020-01-17 12:14:02 +0100

    gei

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola buenas tardes");
         System.out.println("Soy Juan");
+        System.out.println("Ivan es gei");
     }
     
 }
